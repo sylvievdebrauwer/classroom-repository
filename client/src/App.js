747d1c4ae084af2917bfcfb29ee662b6960e572e
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2023-12-18 14:32:18 +0100

    Inde .js changes

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label>Idea 1</label>
+      <input type ="text"/>
+    </div>
+  );
+}
+
+export default App;
+
+
