22867fccce08936f9126e632c3e6c072a1c1a3f7
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2023-12-19 20:15:49 +0100

    Changes to server.js

--- client/app.js
+++ client/app.js
@@ -1,11 +1,8 @@
     // This code will run once the DOM (HTML) is fully loaded
     console.log('Frontend JavaScript loaded.');
-  
-    // You can make a fetch request to your server to check the database connection
-    fetch('http://localhost:5500/check-database-connection')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Database connection status:', data.status);
-        // You can further handle the response based on your needs
-      })
-      .catch(error => console.error('Error checking database connection:', error));
+
+    (async () => {
+        const response = await fetch('http://localhost:3000/amazing_ideas');
+        const data = await response.json();
+        console.log(data);
+      })();
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import {config} from "dotenv";
 import cors from 'cors';
 import mariadb from 'mariadb';
 
-
+config()
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
-const app = express();
+
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
@@ -18,18 +19,42 @@ const pool = mariadb.createPool({
     connectionLimit: 5
 });
 
+const app = express();
+// Error in console log to improve the performance of the web page when adding events
+// Example for 'touchstart' event
+element.addEventListener('touchstart', handleTouchStart, { passive: true });
+// Example for 'touchmove' event
+element.addEventListener('touchmove', handleTouchMove, { passive: true });
+// Example for 'mousewheel' event
+element.addEventListener('mousewheel', handleMouseWheel, { passive: true });
+
+
+app.listen(3000, async () => {
+  console.log('Server started: http://localhost:3000/');
+  let connection 
+
+  try {
+      connection = await pool.getConnection()
+      const data = await connection.query("SELECT * FROM amazing_ideas");
+
+      console.log(data)
+  } catch (err) {
+    throw error;
+
+  } finally {
+    if (connection) connection.end();
+  }
+});
+
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json(/*{ limit: '50mb' }*/));
 
 app.get('/', async (req, res) => {
     let connection;
     try {
         connection = await pool.getConnection();
         console.log('connected to database');
-        const data = await connection.query(
-            "SELECT * FROM amazing_ideas"
-            
-        );
+        const data = await connection.query("SELECT * FROM amazing_ideas");
         console.log(data)
         res.send(data);
         } catch (error) {
@@ -37,6 +62,28 @@ app.get('/', async (req, res) => {
         } finally {
             if (connection) connection.end();
         }
+});
+
+// Add a route for /amazing_ideas without an ID
+app.get('/amazing_ideas', async (req, res) => {
+  let connection;
+  try {
+      connection = await pool.getConnection();
+      console.log('connected to database');
+      const data = await connection.query('SELECT * FROM amazing_ideas');
+      console.log(data);
+      res.send(data);
+  } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+      if (connection) connection.end();
+  }
+});
+
+// Your existing route for /amazing_ideas/:id
+app.get("/amazing_ideas/:id", async (req, res) => {
+  // ...
 });
 
 app.get("/amazing_ideas/:id", async (req, res) => {
@@ -55,7 +102,7 @@ app.get("/amazing_ideas/:id", async (req, res) => {
     }
 });
 
-// POST endpoint to add a new idea
+// POST endpoint to add a new idea... USE PREPARE
 app.post('/amazing_ideas', async (req, res) => {
     const { title, description } = req.body;
   
@@ -80,7 +127,7 @@ app.post('/amazing_ideas', async (req, res) => {
   });
   
 
-app.listen(PORT, () => console.log('Server started: http:localhost:${PORT}/'))
+//app.listen(PORT, () => console.log('Server started: http:localhost:${PORT}/'))
 
 
 
